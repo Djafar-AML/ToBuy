02c56154adafffae63238217bd6a1baa960602f4
Jeff <Jafaragha77@gmail.com>
2023-03-01 15:10:21 +0330

    - destination change listener for nav controller

--- app/src/main/java/com/example/tobuy/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/tobuy/ui/activity/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.tobuy.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.tobuy.R
 import com.example.tobuy.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,15 +17,29 @@ class MainActivity : AppCompatActivity() {
 
     val navController by lazy { navController() }
 
+    private val topLevelDestinationIds by lazy { setOf(R.id.homeFragment, R.id.profileFragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         setupBottomNavigation()
+        setDestinationListenerForNavController()
     }
 
     private fun setupBottomNavigation() {
         val bottomNavigation = binding.bottomNavigation
         setupWithNavController(bottomNavigation, navController)
+    }
+
+
+    private fun setDestinationListenerForNavController() {
+
+        val bottomNavigation = binding.bottomNavigation
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            bottomNavigation.isGone = topLevelDestinationIds.contains(destination.id).not()
+        }
+
     }
 
     private fun navController(): NavController {
